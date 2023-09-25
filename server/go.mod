e1e9a3be7babeb1752e5037d003d437c107b835a
Ara-O <oladipoeyiara@gmail.com>
2023-09-25 10:15:04 -0400

    Setting up JWTs

--- server/go.mod
+++ server/go.mod
@@ -5,19 +5,30 @@ go 1.21.0
 require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/cors v1.2.1
+	github.com/go-chi/jwtauth/v5 v5.1.1
 	github.com/go-playground/validator/v10 v10.15.4
 	github.com/joho/godotenv v1.5.1
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.0.11 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
--- server/handlers/registration/sendConfirmationEmail.go
+++ server/handlers/registration/sendConfirmationEmail.go
@@ -6,18 +6,19 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
-	"gopkg.in/gomail.v2"
+	"github.com/mitchellh/mapstructure"
 )
 
 type UserData struct {
-	FirstName        string `json:"firstName" validate:"required,min=2,max=50"`
-	LastName         string `json:"lastName" validate:"required,min=2,max=50"`
-	PhoneNumber      int64  `json:"phoneNumber" validate:"required,number"`
-	WorkEmail        string `json:"workEmail" validate:"required,alphanum"`
-	EmploymentNumber int64  `json:"employmentNumber" validate:"required,number"`
-	Department       string `json:"department" validate:"required"`
+	FirstName        string `json:"firstName" validate:"required,min=2,max=50" mapstructure:"firstName"`
+	LastName         string `json:"lastName" validate:"required,min=2,max=50" mapstructure:"lastName"`
+	PhoneNumber      int64  `json:"phoneNumber" validate:"required,number" mapstructure:"phoneNumber"`
+	WorkEmail        string `json:"workEmail" validate:"required,alphanum" mapstructure:"workEmail"`
+	EmploymentNumber int64  `json:"employmentNumber" validate:"required,number" mapstructure:"employmentNumber"`
+	Department       string `json:"department" validate:"required" mapstructure:"department"`
 }
 
 func SendConfirmationEmail(w http.ResponseWriter, r *http.Request) {
@@ -47,30 +48,45 @@ func SendConfirmationEmail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Create and send a new message
-	m := gomail.NewMessage()
-	m.SetHeader("From", "UDM Reimbursement Team <ara@araoladipo.dev>")
-	m.SetHeader("To", "oladipea@udmercy.edu")
-	m.SetHeader("Subject", "Verify your UDM Email")
+	//Create JWTs
+	var tokenAuth *jwtauth.JWTAuth
 
-	mailTemplate := `
-		<div style="background: white">
-			<h2 style="font-weight: 500">Verify your Account</h2>
-			<h4 style="font-weight: 300">Thanks for signing up for the University of Detroit Mercy Reimbursement System!</h4>
-			<h4 style="font-weight: 300">You can verify your account with this link</h4>
-			<a href="https://araoladipo.dev"><button style="font-weight: 300; color: white; text-decoration: none; background: #a5093e; padding: 7px 20px">Here</button></a>
-			</div>
-	`
-	// Set the email body
-	m.SetBody("text/html", mailTemplate)
+	tokenAuth = jwtauth.New("HS256", []byte(os.Getenv("JWT_TOKEN_KEY")), nil)
 
-	// Create a new SMTP client
-	d := gomail.NewDialer(os.Getenv("SENDGRID_URL"), 587, os.Getenv("SENDGRID_USERNAME"), os.Getenv("SENDGRID_PASSWORD"))
-
-	// Send the email
-	if err := d.DialAndSend(m); err != nil {
+	formattedUserData := make(map[string]interface{})
+	if err := mapstructure.Decode(userData, &formattedUserData); err != nil {
 		fmt.Println(err)
 	}
+
+	_, tokenString, err := tokenAuth.Encode(formattedUserData)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("DEBUG: a sample jwt is %s\n\n", tokenString)
+	// Create and send a new message
+	// m := gomail.NewMessage()
+	// m.SetHeader("From", "UDM Reimbursement Team <ara@araoladipo.dev>")
+	// m.SetHeader("To", "oladipea@udmercy.edu")
+	// m.SetHeader("Subject", "Verify your UDM Email")
+
+	// mailTemplate := `
+	// 	<div style="background: white">
+	// 		<h2 style="font-weight: 500">Verify your Account</h2>
+	// 		<h4 style="font-weight: 300">Thanks for signing up for the University of Detroit Mercy Reimbursement System!</h4>
+	// 		<h4 style="font-weight: 300">You can verify your account with this link</h4>
+	// 		<a href="https://araoladipo.dev"><button style="font-weight: 300; color: white; text-decoration: none; background: #a5093e; padding: 7px 20px">Here</button></a>
+	// 		</div>
+	// `
+	// // Set the email body
+	// m.SetBody("text/html", mailTemplate)
+
+	// // Create a new SMTP client
+	// d := gomail.NewDialer(os.Getenv("SENDGRID_URL"), 587, os.Getenv("SENDGRID_USERNAME"), os.Getenv("SENDGRID_PASSWORD"))
+
+	// // Send the email
+	// if err := d.DialAndSend(m); err != nil {
+	// 	fmt.Println(err)
+	// }
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Email Sent!"))
